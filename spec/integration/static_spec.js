5eefe0ae6fc27f1424fa65c8d7ec72af736eaf09
Pierce Hicks <phicks21@gmail.com>
2019-06-28 06:05:42 -0400

    app views up 4.1

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,31 +1,16 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
-const marcoBase = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
   describe("GET /", () => {
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
+
         done();
       });
     });
   });
-
-  describe("GET /", () => {
-   it("should return status code 200", (done) => {
-     request.get(marcoBase, (err, res, body) => {
-       expect(res.statusCode).toBe(200);
-       done();
-     });
-   });
-
-   it("should return the string 'polo'", (done) => {
-     request.get(marcoBase, (err, res, body) => {
-       expect(body).toContain('polo');
-       done();
-     });
-   });
- });
 });
