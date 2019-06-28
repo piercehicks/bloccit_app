abcc7b2c431e3e8300ac05327e166066b6821a48
Pierce Hicks <phicks21@gmail.com>
2019-06-28 04:48:36 -0400

    marco polo initiated

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marcoBase = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
   describe("GET /", () => {
@@ -11,4 +12,20 @@ describe("routes : static", () => {
       });
     });
   });
+
+  describe("GET /", () => {
+   it("should return status code 200", (done) => {
+     request.get(marcoBase, (err, res, body) => {
+       expect(res.statusCode).toBe(200);
+       done();
+     });
+   });
+
+   it("should return the string 'polo'", (done) => {
+     request.get(marcoBase, (err, res, body) => {
+       expect(body).toContain('polo');
+       done();
+     });
+   });
+ });
 });
