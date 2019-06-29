18acbcb6877846ea65d36e06f30e28de33e0297a
Pierce Hicks <phicks21@gmail.com>
2019-06-29 14:08:25 -0400

    topic 2 complete iss 6.1

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -1,3 +1,4 @@
+
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/topics/";
@@ -5,40 +6,138 @@ const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 
 describe("routes : topics", () => {
-    beforeEach((done) => {
-        this.topic;
-        sequelize.sync({force: true}).then((res) => {
-
-         Topic.create({
-           title: "JS Frameworks",
-           description: "There is a lot of them"
-         })
-          .then((topic) => {
-            this.topic = topic;
-            done();
-          })
-          .catch((err) => {
-            console.log(err);
-            done();
-          });
-
+  beforeEach(done => {
+    this.topic;
+    sequelize.sync({ force: true }).then(res => {
+      Topic.create({
+        title: "JS Frameworks",
+        description: "There is a lot of them"
+      })
+        .then(topic => {
+          this.topic = topic;
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done();
         });
-
-      });
+    });
+  });
 
   describe("GET /topics", () => {
+    it("should return a status code 200 and all topics", done => {
+      //#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(err).toBeNull();
+        expect(body).toContain("Topics");
+        expect(body).toContain("JS Frameworks");
+        done();
+      });
+    });
+  });
 
-    it("should return a status code 200 and all topics", (done) => {
+  describe("GET /topics/new", () => {
+    it("should render a new topic form", done => {
+      request.get(`${base}new`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("New Topic");
+        done();
+      });
+    });
+  });
 
-               request.get(base, (err, res, body) => {
-                 expect(res.statusCode).toBe(200);
-                 expect(err).toBeNull();
-                 expect(body).toContain("Topics");
-                 expect(body).toContain("JS Frameworks");
-                 done();
-               });
-             });
-           });
+  describe("POST /topics/create", () => {
+    const options = {
+      url: `${base}create`,
+      form: {
+        title: "blink-182 songs",
+        description: "What's your favorite blink-182 song?"
+      }
+    };
 
+    it("should create a new topic and redirect", done => {
+      request.post(
+        options,
 
+        (err, res, body) => {
+          Topic.findOne({ where: { title: "blink-182 songs" } })
+            .then(topic => {
+              expect(res.statusCode).toBe(303);
+              expect(topic.title).toBe("blink-182 songs");
+              expect(topic.description).toBe(
+                "What's your favorite blink-182 song?"
+              );
+              done();
+            })
+            .catch(err => {
+              console.log(err);
+              done();
+            });
+        }
+      );
+    });
+  });
+
+  describe("GET /topics/:id", () => {
+    it("should render a view with the selected topic", done => {
+      request.get(`${base}${this.topic.id}`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("JS Frameworks");
+        done();
+      });
+    });
+  });
+
+  describe("POST /topics/:id/destroy", () => {
+    it("should delete the topic with the associated ID", done => {
+      Topic.findAll().then(topics => {
+        const topicCountBeforeDelete = topics.length;
+
+        expect(topicCountBeforeDelete).toBe(1);
+
+        request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
+          Topic.findAll().then(topics => {
+            expect(err).toBeNull();
+            expect(topics.length).toBe(topicCountBeforeDelete - 1);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe("GET /topics/:id/edit", () => {
+    it("should render a view with an edit topic form", done => {
+      request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Edit Topic");
+        expect(body).toContain("JS Frameworks");
+        done();
+      });
+    });
+  });
+
+  describe("POST /topics/:id/update", () => {
+    it("should update the topic with the given values", done => {
+      const options = {
+        url: `${base}${this.topic.id}/update`,
+        form: {
+          title: "JS Frameworks",
+          description: "There are a lot of them"
+        }
+      };
+
+      request.post(options, (err, res, body) => {
+        expect(err).toBeNull();
+
+        Topic.findOne({
+          where: { id: this.topic.id }
+        }).then(topic => {
+          expect(topic.title).toBe("JS Frameworks");
+          done();
+        });
+      });
+    });
+  });
 });
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,17 +1,64 @@
 const Topic = require("./models").Topic;
 
 module.exports = {
-
-//#1
-  getAllTopics(callback){
-    return Topic.findAll()
-
-//#2
-    .then((topics) => {
-      callback(null, topics);
+  getAllTopics(callback) {
+    return (
+      Topic.findAll()
+        .then(topics => {
+          callback(null, topics);
+        })
+        .catch(err => {
+          callback(err);
+        })
+    );
+  },
+  addTopic(newTopic, callback){
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then((topic) => {
+      callback(null, topic);
     })
     .catch((err) => {
       callback(err);
     })
-  }
-}
+  },
+   getTopic(id, callback){
+     return Topic.findByPk(id)
+     .then((topic) => {
+       callback(null, topic);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+   },
+   deleteTopic(id, callback){
+     return Topic.destroy({
+       where: {id}
+     })
+     .then((topic) => {
+       callback(null, topic);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+   },
+   updateTopic(id, updatedTopic, callback){
+     return Topic.findById(id)
+     .then((topic) => {
+       if(!topic){
+         return callback("Topic not found");
+       }
+       topic.update(updatedTopic, {
+         fields: Object.keys(updatedTopic)
+       })
+       .then(() => {
+         callback(null, topic);
+       })
+       .catch((err) => {
+         callback(err);
+       });
+     });
+   }
+};
