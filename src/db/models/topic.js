795a93b63b51703c7abe76bfb4ee69dc48a57628
Pierce Hicks <phicks21@gmail.com>
2019-06-29 11:41:51 -0400

    Rule model added

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -4,6 +4,12 @@ module.exports = (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     description: DataTypes.STRING
   }, {});
+  Topic.associate = function(models){
+   Topic.hasMany(models.Rule, {
+     foreignKey: "topicId",
+     as: "rules"
+   });
+ }
   Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
