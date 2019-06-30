2a8ba38f21379d0b862fa93de275cbe2a552d419
Pierce Hicks <phicks21@gmail.com>
2019-06-30 18:04:09 -0400

    corrected Static spec Seeded app

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,7 +4,7 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
@@ -12,18 +12,5 @@ describe("routes : static", () => {
         done();
       });
     });
-  });
-
-  describe("GET /about", () => {
-
-      it("should return status code 200 and have 'About us' in the body of the response", (done) => {
-
-          request.get(base, (err, res, body) => {
-              expect(res.statusCode).toBe(200);
-              expect(body).toContain("About Us");
-
-              done();
-          });
-      });
   });
 });
