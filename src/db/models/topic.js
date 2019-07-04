e1a8af83e0ca8d082084a2b084e59f72cc77d475
Pierce Hicks <phicks21@gmail.com>
2019-07-04 12:06:44 -0400

    Implemented Post Model

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+       type: DataTypes.STRING,
+       allowNull: false
+     },
+    description: {
+       type: DataTypes.STRING,
+       allowNull: false
+     }
   }, {});
   Topic.associate = function(models){
    Topic.hasMany(models.Rule, {
@@ -16,6 +22,11 @@ module.exports = (sequelize, DataTypes) => {
     foreignKey: "topicId",
     as: "banners",
   });
+
+  Topic.hasMany(models.Post, {
+       foreignKey: "topicId",
+       as: "posts"
+     });
   };
   return Topic;
 };
