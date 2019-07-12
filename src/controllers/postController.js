4ad39a7ab1e4f04511f1c49b1d561efc73f6c07b
Pierce Hicks <phicks21@gmail.com>
2019-07-12 17:27:21 -0400

    Fixed routes and missed merge

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -31,10 +31,7 @@ module.exports = {
  destroy(req, res, next) {
     postQueries.deletePost(req.params.id, (err, deletedRecordsCount) => {
       if (err) {
-        res.redirect(
-          500,
-          `/topics/${req.params.topicId}/posts/${req.params.id}`
-        );
+        res.redirect(500,`/topics/${req.params.topicId}/posts/${req.params.id}`)
       } else {
         res.redirect(303, `/topics/${req.params.topicId}`);
       }
