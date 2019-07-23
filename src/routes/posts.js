4d4e038950a142809dcd6e44734d6f60d572f2c0
Pierce Hicks <phicks21@gmail.com>
2019-07-22 20:32:08 -0400

    fixed post route

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -12,6 +12,7 @@ router.post(
   postController.create
 );
 router.get("/topics/:topicId/posts/:id", postController.show);
+router.post("/topics/:topicId/posts/:id/update", postController.update);
 router.post("/topics/:topicId/posts/:id/destroy", postController.destroy);
 router.get("/topics/:topicId/posts/:id/edit", postController.edit);
 router.post(
