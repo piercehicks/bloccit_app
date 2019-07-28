9d9d2c9c68695a30db85ed419500a90e70d8fd04
Pierce Hicks <phicks21@gmail.com>
2019-07-28 09:58:46 -0400

    upvote now only +1

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -1,39 +1,31 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  var Post = sequelize.define(
-    "Post",
-    {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      body: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-
-      topicId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      }
-    },
-    {}
-  );
-  Post.associate = function(models) {
+    var Post = sequelize.define(
+        "Post",
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            body: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            topicId: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        },
+        {}
+    );
+    Post.associate = function(models) {
         Post.belongsTo(models.Topic, {
             foreignKey: "topicId",
             onDelete: "CASCADE"
-        });
-        Post.afterCreate((post, callback) => {
-            return models.Vote.create({
-                value: 1,
-                userId: post.userId,
-                postId: post.id
-            });
         });
         Post.belongsTo(models.User, {
             foreignKey: "userId",
@@ -67,32 +59,19 @@ module.exports = (sequelize, DataTypes) => {
      });
     };
 
-Post.prototype.getPoints = function() {
-   if (this.votes.length === 0) return 0;
+    Post.prototype.getPoints = function() {
+        if (this.votes.length === 0) return 0;
 
-   return this.votes
-       .map(v => {
-           return v.value;
-       })
-       .reduce((prev, next) => {
-           return prev + next;
-       });
+        return this.votes
+            .map(v => {
+                return v.value;
+            })
+            .reduce((prev, next) => {
+                return prev + next;
+            });
+    };
 
-  };
-  Post.prototype.hasUpvoteFor = function(userId) {
-    const foundUpvote = this.votes.filter(vote => {
-      return vote.value === 1 && vote.userId == userId;
-    });
-    return foundUpvote.length === 1;
-  };
-
-  Post.prototype.hasDownvoteFor = function(userId) {
-    const foundDownvote = this.votes.filter(vote => {
-      return vote.value === -1 && vote.userId == userId;
-    });
-    return foundDownvote.length === 1;
-  };
-  Post.prototype.getFavoriteFor = function(userId) {
+    Post.prototype.getFavoriteFor = function(userId) {
         return this.favorites.find(favorite => {
             return favorite.userId == userId;
         });
@@ -104,5 +83,5 @@ Post.prototype.getPoints = function() {
             order: [["createdAt", "DESC"]]
           }
         });
-  return Post;
+    return Post;
 };
